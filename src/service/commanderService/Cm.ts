40e03557ac820c7acce64ff40add2007e24514cf
wangshuai <wangsbruce@163.com>
2024-05-16 15:33:13 +0800

    fix: 测试

--- src/service/commanderService/Cm.ts
+++ src/service/commanderService/Cm.ts
@@ -19,7 +19,7 @@ export default class CmCommand {
         })
         if (!_cmInfo || !checkExecaInfoNoError(_cmInfo)) return
         const choosePush = await InquirerService.confirm('是否推送到远端分支?', true)
-        console.log(choosePush, '选择推送---')
+        console.log(choosePush, '选择推送----')
       })
     return command
   }
