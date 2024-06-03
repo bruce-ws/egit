37a54285fa0b70d7cdfd3f877711204e471c1821
wangshuai <wangsbruce@163.com>
2024-06-03 17:02:30 +0800

    feat: rb

--- src/service/commanderService/Rb.ts
+++ src/service/commanderService/Rb.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander'
 import GitInvoker from '@/CMDInvoke/index'
-// import { checkExecaInfoNoError, outputRes } from '@/utils/index'
+import { checkExecaInfoNoError, outputRes } from '@/utils/index'
 export class RbCommand {
   createCommand(): Command {
     return new Command('rebase')
@@ -9,10 +9,18 @@ export class RbCommand {
       .argument('<branch>', '要删除的分支')
       .action(async (branchName: string) => {
         const gitInvoker = new GitInvoker()
+        // 1：拉取远端最新代码
+        outputRes(`开始拉取 ${branchName} 分支远端最新代码`, 51)
         const _fetchInfo = await gitInvoker.executeCmd('fetch', {
           branch: branchName,
         })
-        console.log(_fetchInfo, '-fetch------')
+        if (!_fetchInfo || !checkExecaInfoNoError(_fetchInfo)) return
+        outputRes(`拉取远端代码成功，开始执行变基操作`, 51)
+        // 2: 开始执行变基操作 注：是基于 origin/branchName 非本地branchName分支
+        const _rebaseInfo = await gitInvoker.executeCmd('rebase', {
+          branch: `origin/${branchName}`,
+        })
+        console.log(_rebaseInfo, 'rebase------info')
       })
   }
 }
