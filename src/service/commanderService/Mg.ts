173e94a5e42a569791d231f823b87ff5a481faaf
wangshuai <wangsbruce@163.com>
2024-06-11 10:23:17 +0800

    feat: mg选择性分支\pu命令

--- src/service/commanderService/Mg.ts
+++ src/service/commanderService/Mg.ts
@@ -1,17 +1,19 @@
 import { Command } from 'commander'
 import GitInvoker from '@/CMDInvoke/index'
-import { checkExecaInfoNoError, outputRes } from '@/utils/index'
+import { checkExecaInfoNoError, outputRes, pullLocalBranch } from '@/utils/index'
 export class MgCommand {
   createCommand(): Command {
-    return new Command('merge')
-      .alias('mg')
-      .argument('<branch>', '要合并的分支')
-      .action(async (branchName: string) => {
-        const gitInvoker = new GitInvoker()
-        const _mergeInfo = await gitInvoker.executeCmd('merge', { branchName })
-        if (!_mergeInfo || !checkExecaInfoNoError(_mergeInfo)) return
-        outputRes('合并完成')
-      })
+    return new Command('merge').alias('mg').action(async () => {
+      const gitInvoker = new GitInvoker()
+      const branchName = await pullLocalBranch('合并')
+      if (branchName.includes('*')) {
+        outputRes(`请选择其他分支进行merge`, 120, false)
+        return
+      }
+      const _mergeInfo = await gitInvoker.executeCmd('merge', { branchName: branchName.replaceAll(' ', '') })
+      if (!_mergeInfo || !checkExecaInfoNoError(_mergeInfo)) return
+      outputRes('合并完成')
+    })
   }
 }
 
